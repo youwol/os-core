2fd6419bc678ef493916a6cde56893b41e8c22f1
youwol-guillaume <greinisch@youwol.com>
2022-12-06 07:55:18 +0100

    🔖 0.1.5

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -98,7 +98,7 @@ const entries = {
 export const setup = {
     name:'@youwol/os-core',
         assetId:'QHlvdXdvbC9vcy1jb3Jl',
-    version:'0.1.5-wip',
+    version:'0.1.5',
     shortDescription:"Core part of YouWol's Operating System.",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/os-core',
     npmPackage:'https://www.npmjs.com/package/@youwol/os-core',
@@ -133,7 +133,7 @@ export const setup = {
         const parameters = installParameters || {}
         const scripts = [
             ...(parameters.scripts || []),
-            `@youwol/os-core#0.1.5-wip~dist/@youwol/os-core/${entry.name}.js`
+            `@youwol/os-core#0.1.5~dist/@youwol/os-core/${entry.name}.js`
         ]
         const modules = [
             ...(parameters.modules || []),
