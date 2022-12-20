d0b881b03eefe438f770ac4e916af9c3ed1af645
Julien DÉCHARNE <jdecharne@youwol.com>
2022-12-20 11:40:14 +0100

    👽️ pipeline_ts : add @youwol/jest-preset

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    testURL: 'http://localhost:2001',
-    reporters: ['default', 'jest-junit'],
-    modulePathIgnorePatterns: ['/dist'],
-}
